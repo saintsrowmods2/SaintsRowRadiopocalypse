9265c6fd32cb617d84c8978c674d6adaff0fd187
Thomas Jepp <tom@tomjepp.co.uk>
2015-04-04 18:56:41 +0100

    Fix spelling of "apocalypse".

--- pack_release.cmd
+++ pack_release.cmd
@@ -5,20 +5,20 @@ echo Packaging revision %1...
 rmdir /s /q packaging_temp 1>NUL 2>NUL
 mkdir packaging_temp 1>NUL 2>NUL
 cd packaging_temp
-mkdir SaintsRowRadiopocalpyse-rev%1 1>NUL 2>NUL
-copy ..\license.txt SaintsRowRadiopocalpyse-rev%1\ 1>NUL 2>NUL
+mkdir SaintsRowRadiopocalypse-rev%1 1>NUL 2>NUL
+copy ..\license.txt SaintsRowRadiopocalypse-rev%1\ 1>NUL 2>NUL
 
-mkdir SaintsRowRadiopocalpyse-rev%1\RadioSwapper 1>NUL 2>NUL
-copy /y ..\bin\Release\*.exe SaintsRowRadiopocalpyse-rev%1\RadioSwapper\ 1>NUL 2>NUL
-copy /y ..\bin\Release\*.dll SaintsRowRadiopocalpyse-rev%1\RadioSwapper\ 1>NUL 2>NUL
-copy /y ..\bin\Release\*.template SaintsRowRadiopocalpyse-rev%1\RadioSwapper\ 1>NUL 2>NUL
-xcopy /y /c /i /s /e ..\bin\Release\wwise SaintsRowRadiopocalpyse-rev%1\RadioSwapper\wwise 1>NUL 2>NUL
+mkdir SaintsRowRadiopocalypse-rev%1\RadioSwapper 1>NUL 2>NUL
+copy /y ..\bin\Release\*.exe SaintsRowRadiopocalypse-rev%1\RadioSwapper\ 1>NUL 2>NUL
+copy /y ..\bin\Release\*.dll SaintsRowRadiopocalypse-rev%1\RadioSwapper\ 1>NUL 2>NUL
+copy /y ..\bin\Release\*.template SaintsRowRadiopocalypse-rev%1\RadioSwapper\ 1>NUL 2>NUL
+xcopy /y /c /i /s /e ..\bin\Release\wwise SaintsRowRadiopocalypse-rev%1\RadioSwapper\wwise 1>NUL 2>NUL
 
-mkdir SaintsRowRadiopocalpyse-rev%1\RadioEnabler 1>NUL 2>NUL
-copy ..\RadioEnabler\bin\Release\*.dll SaintsRowRadiopocalpyse-rev%1\RadioEnabler\ 1>NUL 2>NUL
-copy ..\RadioEnabler\RadioEnabler.ini SaintsRowRadiopocalpyse-rev%1\RadioEnabler\ 1>NUL 2>NUL
+mkdir SaintsRowRadiopocalypse-rev%1\RadioEnabler 1>NUL 2>NUL
+copy ..\RadioEnabler\bin\Release\*.dll SaintsRowRadiopocalypse-rev%1\RadioEnabler\ 1>NUL 2>NUL
+copy ..\RadioEnabler\RadioEnabler.ini SaintsRowRadiopocalypse-rev%1\RadioEnabler\ 1>NUL 2>NUL
 
-"C:\Program Files\7-Zip\7z.exe" a ..\SaintsRowRadiopocalpyse-rev%1.zip SaintsRowRadiopocalpyse-rev%1
+"C:\Program Files\7-Zip\7z.exe" a ..\SaintsRowRadiopocalypse-rev%1.zip SaintsRowRadiopocalypse-rev%1
 cd ..
 rmdir /s /q packaging_temp 1>NUL 2>NUL
 echo Done!
