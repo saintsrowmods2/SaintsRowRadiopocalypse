c9545517cabec85e2eeb0470ae14660f3a11faad
Thomas Jepp <tom@tomjepp.co.uk>
2017-05-11 18:48:24 +0100

    Update ThomasJepp.SaintsRow, tidy up RedirectIOToConsole.

--- RadioEnabler/code/dll.cpp
+++ RadioEnabler/code/dll.cpp
@@ -50,10 +50,7 @@ BOOL APIENTRY DllMain(
 		{
 			ConfigLoad();
 
-			if (ShouldCreateConsole)
-			{
-				RedirectIOToConsole();
-			}
+			RedirectIOToConsole();
 
 			OpenLog();
 
@@ -93,35 +90,15 @@ BOOL APIENTRY DllMain(
 
 VOID WINAPI RedirectIOToConsole()
 {
-	int hConHandle;
-	long lStdHandle;
-	CONSOLE_SCREEN_BUFFER_INFO coninfo;
-	FILE *fp;
-	// allocate a console for this app
-	AllocConsole();
-	// set the screen buffer to be big enough to let us scroll text
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-	coninfo.dwSize.Y = 1000;
-	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-	// redirect unbuffered STDOUT to the console
-	lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-	fp = _fdopen(hConHandle, "w");
-	*stdout = *fp;
-	setvbuf(stdout, NULL, _IONBF, 0);
-	// redirect unbuffered STDIN to the console
-	lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-	fp = _fdopen(hConHandle, "r");
-	*stdin = *fp;
-	setvbuf(stdin, NULL, _IONBF, 0);
-	// redirect unbuffered STDERR to the console
-	lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-	fp = _fdopen(hConHandle, "w");
-	*stderr = *fp;
-	setvbuf(stderr, NULL, _IONBF, 0);
-	// make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog 
-	// point to console as well
-	std::ios::sync_with_stdio();
+	FILE *conin, *conout;
+	// Allocate and attach console
+	if (ShouldCreateConsole)
+	{
+		AllocConsole();
+		freopen_s(&conin, "conin$", "r", stdin);
+		freopen_s(&conout, "conout$", "w", stdout);
+		freopen_s(&conout, "conout$", "w", stderr);
+	}
+
+	printf("test!\n");
 }
